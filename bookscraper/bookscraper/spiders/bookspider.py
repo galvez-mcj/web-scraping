d20097da0d2cd55ffcf75c1c05d8e81189272842
tinay <galvez.mcj@gmail.com>
2023-05-26 17:25:44 +0800

    added code to scrape other succeeding pages

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,21 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield {
+                'name': book.css('h3 a::attr(title)').get(),
+                'price': book.css('.product_price .price_color::text').get(),
+                'url': book.css('h3 a').attrib['href']
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        
+        # Check to see if current page's next_page is not None
+        if next_page:
+            if 'catalogue/' in next_page:   # for page 1 to 2 only
+                next_page_url = 'http://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'http://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
